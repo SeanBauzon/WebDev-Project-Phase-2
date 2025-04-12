1a95d2e6e6be060ab72fca18976506ad35f267d1
SohanChh <167275645+SohanChh@users.noreply.github.com>
2025-04-12 15:50:50 -0600

    Update page.js

--- phase2/app/page.js
+++ phase2/app/page.js
@@ -3,6 +3,7 @@ import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import ItemList from "./components/itemlist";
 import SearchBar from "./components/searchbar";
+import FilterPanel from "./components/FilterPanel";
 
 export default function Layout({ children }) {
   return (
