2dbc2c965bd0e82894c880c52b29a84c0d11f4e5
sean yo <seanizaac.bauzon@sait.edu.ca>
2025-04-13 22:44:56 -0600

    sds

--- phase2/app/ProfilePage/page.js
+++ phase2/app/ProfilePage/page.js
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
 
 export default function ProfilePage() {
   const [user, setUser] = useState(null);
